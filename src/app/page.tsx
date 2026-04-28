a64faf7f62ded43297646b219e88834809542986
robonexx <robertwagar@gmail.com>
2026-04-28 19:12:54 +0200

    error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <Hero
-          esktopTitleLines={['FUNKCAMP', '2027']}
+          desktopTitleLines={['FUNKCAMP', '2027']}
           mobileTitleLines={['FUNK', 'CAMP', '2027']}
           subtitle='Best of the best'
           imageSrc='/images/hero-players.png'
